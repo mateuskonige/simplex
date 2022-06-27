cb15f3a3deddcec7346e9fe2b725d04b11e4d4b7
Mateus Reis <you@example.com>
2022-06-27 15:52:16 -0300

    javascript variables

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,8 @@
                         <v-card-text>
                             <v-row>
                                 <v-col>
-                                    <v-select :items="items" v-model="form.to"> </v-select>
+                                    <v-select :items="items" v-model.number="form.to">
+                                    </v-select>
                                 </v-col>
                                 <v-col>
                                     <v-text-field
@@ -16,36 +17,69 @@
                                     ></v-text-field>
                                 </v-col>
                                 <v-col>
-                                    <v-text-field label="X1" v-model="form.f[0]"></v-text-field>
+                                    <v-text-field
+                                        label="X1"
+                                        v-model.number="form.f[0]"
+                                    ></v-text-field>
                                 </v-col>
                                 <v-col>
-                                    <v-text-field label="X2"  v-model="form.f[1]"></v-text-field>
+                                    <v-text-field
+                                        label="X2"
+                                        v-model.number="form.f[1]"
+                                    ></v-text-field>
                                 </v-col>
                             </v-row>
                             <v-row>
                                 <v-col> S.a. </v-col>
                             </v-row>
-                            <div v-for="(i, index) in form.constrained.length" :key="index">
-                            <v-row>
-                                <v-col>
-                                    <v-text-field type="number" label="X1" v-model="form.constrained[index].vars[0]"></v-text-field>
-                                </v-col>
-                                <v-col>
-                                    <v-text-field type="number" label="X2" v-model="form.constrained[index].vars[1]"></v-text-field>
-                                </v-col>
-                                <v-col>
-                                    <v-select :items="to" v-model="form.constrained[index].to"> </v-select>
-                                </v-col>
-                                <v-col>
-                                    <v-text-field type="number" v-model="form.constrained[index].const"></v-text-field>
-                                </v-col>
-                            </v-row>
+                            <div
+                                v-for="(i, index) in form.constrained.length"
+                                :key="index"
+                            >
+                                <v-row>
+                                    <v-col>
+                                        <v-text-field
+                                            type="number" step="0.1"
+                                            label="X1"
+                                            v-model.number="
+                                                form.constrained[index].vars[0]
+                                            "
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col>
+                                        <v-text-field
+                                            type="number" step="0.1"
+                                            label="X2"
+                                            v-model.number="
+                                                form.constrained[index].vars[1]
+                                            "
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col>
+                                        <v-select
+                                            :items="to"
+                                            v-model="form.constrained[index].to"
+                                        >
+                                        </v-select>
+                                    </v-col>
+                                    <v-col>
+                                        <v-text-field
+                                            type="number" step="0.1"
+                                            v-model.number="
+                                                form.constrained[index].const
+                                            "
+                                        ></v-text-field>
+                                    </v-col>
+                                </v-row>
                             </div>
-
                         </v-card-text>
                         <v-card-actions>
                             <v-spacer />
-                            <v-btn type="submit" class="align-right" color="primary" depressed
+                            <v-btn
+                                type="submit"
+                                class="align-right"
+                                color="primary"
+                                depressed
                                 >Calcular</v-btn
                             >
                         </v-card-actions>
@@ -55,14 +89,22 @@
                     <v-card-text>
                         <v-row>
                             <v-col>
-                                {{ form.to }} Z = {{form.f[0]}}x1 {{form.f[1]}}x2 <br>
-                                S.a. <br>
-                               <div v-for="(i, index) in form.constrained.length" :key="index">
-                                {{form.constrained[index].vars[0]}}x1 {{form.constrained[index].vars[1]}}x2 {{form.constrained[index].to}} {{form.constrained[index].const}}
-                                {{form.constrained[index].folga}}
-                                {{form.constrained[index].artificial}}
-                                {{form.constrained[index].excesso}}
-                               </div>
+                                {{ form.to }} Z = {{ form.f[0] }}x1
+                                {{ form.f[1] }}x2 <br />
+                                S.a. <br />
+                                <div
+                                    v-for="(i, index) in form.constrained
+                                        .length"
+                                    :key="index"
+                                >
+                                    {{ form.constrained[index].vars[0] }}x1
+                                    {{ form.constrained[index].vars[1] }}x2
+                                    {{ form.constrained[index].to }}
+                                    {{ form.constrained[index].const }}
+                                    {{ form.constrained[index].folga }}
+                                    {{ form.constrained[index].artificial }}
+                                    {{ form.constrained[index].excesso }}
+                                </div>
                             </v-col>
                         </v-row>
                         <!-- <v-simple-table>
@@ -98,28 +140,99 @@ export default {
             calculated: false,
             form: {
                 f: [0, 0],
-                to: '',
+                to: "",
                 constrained: [
-                    {vars: [0,0], to: '', const: 0, folga: [], artificial: [], excesso: []},
-                    {vars: [0,0], to: '', const: 0, folga: [], artificial: [], excesso: []},
-                    {vars: [0,0], to: '', const: 0, folga: [], artificial: [], excesso: []},
+                    {
+                        vars: [0, 0],
+                        to: "",
+                        const: 0,
+                        folga: [],
+                        artificial: [],
+                        excesso: [],
+                    },
+                    {
+                        vars: [0, 0],
+                        to: "",
+                        const: 0,
+                        folga: [],
+                        artificial: [],
+                        excesso: [],
+                    },
+                    {
+                        vars: [0, 0],
+                        to: "",
+                        const: 0,
+                        folga: [],
+                        artificial: [],
+                        excesso: [],
+                    },
                 ],
-            }
+            },
         };
     },
     methods: {
-        calc(){
+        calc() {
             for (let index = 0; index < this.form.constrained.length; index++) {
-                if (this.form.constrained[index].to == '<=') {
-                    this.form.constrained[index].folga.push(1);
-                } else if(this.form.constrained[index].to == '=') {
-                    this.form.constrained[index].artificial.push(1);
+                if (this.form.constrained[index].to == "<=") {
+                    for (let j = 0; j < this.form.constrained.length; j++) {
+                        if (index == j) {
+                            this.form.constrained[index].folga.push(1);
+                        } else {
+                            this.form.constrained[index].folga.push(0);
+                        }
+                    }
+                } else if (this.form.constrained[index].to == "=") {
+                    for (let j = 0; j < this.form.constrained.length; j++) {
+                        if (index == j) {
+                            this.form.constrained[index].artificial.push(1);
+                        } else {
+                            this.form.constrained[index].artificial.push(0);
+                        }
+                    }
                 } else {
-                    this.form.constrained[index].artificial.push(1);
-                    this.form.constrained[index].excesso.push(-1);
+                    for (let j = 0; j < this.form.constrained.length; j++) {
+                        if (index == j) {
+                            this.form.constrained[index].artificial.push(1);
+                            this.form.constrained[index].excesso.push(-1);
+                        } else {
+                            this.form.constrained[index].artificial.push(0);
+                        }
+                    }
                 }
             }
-        }
+
+
+
+    for (let row = 0; row < this.form.constrained.length; row++) {
+        const linha = [];
+
+                    for (let i = 0; i < this.form.constrained[row].vars.length; i++) {
+                linha.push(this.form.constrained[row].vars[i]);
+
+
+            }
+                    for (let i = 0; i < this.form.constrained[row].folga.length; i++) {
+                linha.push(this.form.constrained[row].folga[i]);
+
+
+            }
+                    for (let i = 0; i < this.form.constrained[row].artificial.length; i++) {
+                linha.push(this.form.constrained[row].artificial[i]);
+
+
+            }
+                    for (let i = 0; i < this.form.constrained[row].excesso.length; i++) {
+                linha.push(this.form.constrained[row].excesso[i]);
+
+
+            }
+
+            console.log(linha);
+
+
     }
+
+        },
+    },
 };
 </script>
