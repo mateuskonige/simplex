c267df7860448802929cdf67b93d25f1bad0eff0
Mateus Reis <mateuskonige@gmail.com>
2022-07-09 17:44:07 -0300

    que sai e que entra

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app dark>
         <v-main>
-            <v-container>
+            <v-container style="max-width: 768px">
                 <v-card elevation="0">
                     <form @submit.prevent="calc()">
                         <v-card-text>
@@ -88,9 +88,13 @@
                         </v-card-actions>
                     </form>
                 </v-card>
+                <div v-if="calculated">
+
+
+                <p>Iteração 1</p>
                 <v-card>
                     <v-card-text >
-                        <v-simple-table dense v-if="calculated">
+                        <v-simple-table dense >
                             <thead>
                                 <tr>
                                     <th>L</th>
@@ -107,32 +111,41 @@
                                 <tr v-for="(lines, i) in linhas" :key="i">
                                     <td>{{ i }}</td>
 
-<td v-for="(li, j) in lines" :key="j">{{ li }}</td>
+                                    <td v-for="(li, j) in lines" :key="j">{{ li }}</td>
                                 </tr>
                             </tbody>
                         </v-simple-table>
-                        <!-- <v-row>
-                            <v-col>
-                                {{ form.to }} Z = {{ form.f[0] }}x1
-                                {{ form.f[1] }}x2 <br />
-                                S.a. <br />
-                                <div
-                                    v-for="(i, index) in form.constrained
-                                        .length"
-                                    :key="index"
-                                >
-                                    {{ form.constrained[index].vars[0] }}x1
-                                    {{ form.constrained[index].vars[1] }}x2
-                                    {{ form.constrained[index].to }}
-                                    {{ form.constrained[index].const }}
-                                    {{ form.constrained[index].folga }}
-                                    {{ form.constrained[index].artificial }}
-                                    {{ form.constrained[index].excesso }}
-                                </div>
-                            </v-col>
-                        </v-row> -->
+
+
                     </v-card-text>
                 </v-card>
+
+                                        <p class="mt-4">Iteração 2</p>
+                                        <v-card >
+
+                        <v-simple-table dense v-if="calculated">
+                            <thead>
+                                <tr>
+                                    <th>L</th>
+                                    <th>x1</th>
+                                    <th>x2</th>
+                                    <th>x3</th>
+                                    <th>x4</th>
+                                    <th>x5</th>
+                                    <th>x6</th>
+                                    <th>b</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(lines, i) in linhas1" :key="i">
+                                    <td>{{ i }}</td>
+
+                                    <td v-for="(li, j) in lines" :key="j">{{ li }}</td>
+                                </tr>
+                            </tbody>
+                        </v-simple-table>
+                                                </v-card>
+                                                 </div>
             </v-container>
         </v-main>
     </v-app>
@@ -146,6 +159,8 @@ export default {
             to: ["<=", "=", ">="],
             calculated: false,
             linhas: [],
+            linhas1: [],
+
             form: {
                 f: [0.4, 0.5],
                 to: "",
@@ -212,8 +227,6 @@ export default {
                             this.form.constrained[index].excesso = -1;
                         } else {
                             this.form.constrained[index].artificial.push(0);
-                                this.form.constrained[index].excesso = 0;
-
                         }
                     }
                 }
@@ -269,7 +282,64 @@ export default {
 
             this.linhas.unshift(linhaZ);
 
+            this.linhas1 = this.linhas;
+
+            //1
             console.log(this.linhas)
+
+            this.linhas1 = JSON.parse(JSON.stringify(this.linhas));
+
+            this.linhas1[0][0] = (this.linhas1[2][0] + this.linhas1[3][0]) * -1
+            this.linhas1[0][1] = (this.linhas1[2][1] + this.linhas1[3][1]) * -1
+
+            //2
+            console.log(this.linhas)
+            //3
+            console.log(this.linhas1)
+
+
+
+            console.log(this.linhas1[0].length);
+            const linhas1linha0positiva = [];
+
+            for (let i = 0; i < this.linhas1[0].length; i++) {
+                linhas1linha0positiva.push(this.linhas1[0][i] * -1)
+            }
+
+            var queEntra = 0;
+
+
+            for (let i = 0; i < this.linhas1[0].length; i++) {
+                if(linhas1linha0positiva[i] < linhas1linha0positiva[i+1])
+                    queEntra = i;
+            }
+
+            console.log(queEntra);
+
+            var queSai = 0;
+
+            let divisao = [];
+            for (let index = 1; index < this.linhas1.length; index++) {
+                divisao.push(parseFloat(this.linhas1[index][6]) / parseFloat(this.linhas1[0][queEntra]));
+
+                for (let index = 0; index < divisao.length; index++) {
+                    if(divisao[index] < 0){
+                        divisao[index] = divisao[index] * -1;
+
+                    }
+                }
+            }
+
+            console.log(divisao);
+
+            for (let index = 0; index < divisao.length; index++) {
+                                if(divisao[index] < divisao[index+1]){
+                                    queSai = index +1;
+                                }
+
+                            }
+
+                console.log(queSai);
         },
     },
 };
